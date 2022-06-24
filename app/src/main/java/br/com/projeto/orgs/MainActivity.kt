c77fc01c9b5f8b01c15fc3abc5b99a72e0d98c5c
Sergio de Oliveira Junior <sergio.sc.tj@gmail.com>
2022-06-23 21:45:34 -0300

    Inserindo Views na Activity

--- app/src/main/java/br/com/projeto/orgs/MainActivity.kt
+++ app/src/main/java/br/com/projeto/orgs/MainActivity.kt
@@ -1,6 +1,17 @@
 package br.com.projeto.orgs
 
 import android.app.Activity
+import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val view = TextView(this)
+        view.setText("Cesta de Frutas")
+        setContentView(view)
+    }
+
 }
