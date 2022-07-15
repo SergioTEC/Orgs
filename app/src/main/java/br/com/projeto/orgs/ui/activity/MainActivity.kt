4999e8d392e2332c1c1720a4e7d34e372060c24e
Sergio de Oliveira Junior <sergio.sc.tj@gmail.com>
2022-07-15 00:27:47 -0300

    Criando o Adapter do RecyclerView

--- app/src/main/java/br/com/projeto/orgs/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/projeto/orgs/ui/activity/MainActivity.kt
@@ -4,7 +4,9 @@ import android.app.Activity
 import android.os.Bundle
 import androidx.recyclerview.widget.RecyclerView
 import br.com.projeto.orgs.R
+import br.com.projeto.orgs.model.Produto
 import br.com.projeto.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
+import java.math.BigDecimal
 
 class MainActivity : Activity() {
 
@@ -18,6 +20,24 @@ class MainActivity : Activity() {
 //        val valor = findViewById<TextView>(R.id.valor)
 //        valor.text = "19.99"
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
-        recyclerView.adapter = ListaProdutosAdapter()
+        recyclerView.adapter = ListaProdutosAdapter(context = this, produtos = listOf(
+            Produto(
+                nome = "Cesta de Frutas",
+                descricao = "Banana, pera, uva",
+                valor = BigDecimal("10.87")
+            ),
+
+            Produto(
+                nome = "Cesta de Frutas 2",
+                descricao = "Melancia, abacate, morango",
+                valor = BigDecimal("14.67")
+            ),
+
+            Produto(
+                nome = "Cesta de Frutas 3",
+                descricao = "Mamao, abacaxi, banana",
+                valor = BigDecimal("14.67")
+            )
+        ))
     }
 }
