2236d3bde13bf45cc932316c88101a4adba2b7b0
Sergio de Oliveira Junior <sergio.sc.tj@gmail.com>
2022-07-17 21:48:34 -0300

    Introdução ao AndroidX

---
+++ app/src/main/java/br/com/projeto/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -0,0 +1,12 @@
+package br.com.projeto.orgs.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.projeto.orgs.R
+
+class FormularioProdutoActivity : AppCompatActivity(R.layout.activity_formulario_produto) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
