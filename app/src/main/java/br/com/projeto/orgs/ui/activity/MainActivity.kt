471f903c7d4f38b1fee1f83097c1b581470e3c65
Sergio de Oliveira Junior <sergio.sc.tj@gmail.com>
2022-07-20 21:33:42 -0300

    Inicializando Activities

--- app/src/main/java/br/com/projeto/orgs/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/projeto/orgs/ui/activity/MainActivity.kt
@@ -1,24 +1,21 @@
 package br.com.projeto.orgs.ui.activity
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.service.controls.actions.FloatAction
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import br.com.projeto.orgs.R
 import br.com.projeto.orgs.model.Produto
 import br.com.projeto.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import java.math.BigDecimal
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        val nome = findViewById<TextView>(R.id.nome)
-//        nome.text = "Cesta de frutas"
-//        val descricao = findViewById<TextView>(R.id.descricao)
-//        descricao.text = "Laranja, manga e uva"
-//        val valor = findViewById<TextView>(R.id.valor)
-//        valor.text = "19.99"
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
         recyclerView.adapter = ListaProdutosAdapter(
             context = this, produtos = listOf(
@@ -41,5 +38,11 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
                 )
             )
         )
+        val fab = findViewById<FloatingActionButton>(R.id.floatingActionButton)
+        fab.setOnClickListener {
+            val intent = Intent(this, FormularioProdutoActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
