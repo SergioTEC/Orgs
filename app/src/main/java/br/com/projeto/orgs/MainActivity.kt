5e8efb837ea97402719bc37df1912fe45fa19227
Sergio de Oliveira Junior <sergio.sc.tj@gmail.com>
2022-10-24 21:58:30 -0300

    Adicionando unidades de medidas no layout, Buscando as views do layout na Activity e Introduzindo o RecyclerView

--- app/src/main/java/br/com/projeto/orgs/MainActivity.kt
+++ app/src/main/java/br/com/projeto/orgs/MainActivity.kt
@@ -2,11 +2,19 @@ package br.com.projeto.orgs
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val nome = findViewById<TextView>(R.id.nome)
+        nome.text = ("Cesta de Frutas")
+        val descricao = findViewById<TextView>(R.id.descricao)
+        descricao.text = ("Laranja, manga e banana")
+        val valor = findViewById<TextView>(R.id.valor)
+        valor.text = ("12.59")
+
     }
 }
