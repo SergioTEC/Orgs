555994d60021649d6f13185581e7536d45bf76ab
Sergio de Oliveira Junior <sergio.sc.tj@gmail.com>
2022-10-31 22:17:09 -0300

    Recursos do AS e configuração de build

---
+++ app/src/main/java/br/com/projeto/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -0,0 +1,9 @@
+package br.com.projeto.orgs.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.projeto.orgs.R
+
+class FormularioProdutoActivity : AppCompatActivity(R.layout.activity_formulario_produto) {
+
+}
--- app/src/main/java/br/com/projeto/orgs/ui/activity/MainActivity.kt
+++ app/src/main/java/br/com/projeto/orgs/ui/activity/MainActivity.kt
@@ -2,17 +2,17 @@ package br.com.projeto.orgs.ui.activity
 
 import android.app.Activity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import br.com.projeto.orgs.R
 import br.com.projeto.orgs.model.Produto
 import br.com.projeto.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
 import java.math.BigDecimal
 
-class MainActivity : Activity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 //        val nome = findViewById<TextView>(R.id.nome)
 //        nome.text = ("Cesta de Frutas")
 //        val descricao = findViewById<TextView>(R.id.descricao)
