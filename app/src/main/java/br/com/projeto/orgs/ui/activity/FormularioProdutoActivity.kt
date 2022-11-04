5dee92a6724af3fd7c0ca1d2f805f029d5dc8e35
Sergio de Oliveira Junior <sergio.sc.tj@gmail.com>
2022-11-03 22:19:01 -0300

    Configurando o listener de clique e Criando o produto no formulário

--- app/src/main/java/br/com/projeto/orgs/ui/activity/FormularioProdutoActivity.kt
+++ app/src/main/java/br/com/projeto/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -2,8 +2,39 @@ package br.com.projeto.orgs.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import br.com.projeto.orgs.R
+import br.com.projeto.orgs.model.Produto
+import java.math.BigDecimal
 
 class FormularioProdutoActivity : AppCompatActivity(R.layout.activity_formulario_produto) {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val botaoSalvar = findViewById<Button>(R.id.botao_salvar)
+        botaoSalvar.setOnClickListener {
+            val campoNome = findViewById<EditText>(R.id.nome)
+            val nome = campoNome.text.toString()
+            val campoDescricao = findViewById<EditText>(R.id.descricao)
+            val descricao = campoDescricao.text.toString()
+            val campoValor = findViewById<EditText>(R.id.valor)
+            val valorEmTexto = campoValor.text.toString()
+            val valor = if (valorEmTexto.isBlank()) {
+                BigDecimal.ZERO
+            } else {
+                BigDecimal(valorEmTexto)
+            }
+
+            val produtoNovo = Produto(
+                nome = nome,
+                descricao = descricao,
+                valor = valor
+            )
+
+            Log.i("FormularioProduto", "onCreate: $produtoNovo")
+        }
+    }
 }
